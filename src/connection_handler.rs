19e3608c78aa790e2181cc44ce3b35ac345aa8df
zacharyoxb <oxboroughzachary@protonmail.com>
2024-05-29 20:59:29 +0100

    finished response handling

--- src/connection_handler.rs
+++ src/connection_handler.rs
@@ -9,52 +9,54 @@ pub(crate) async fn handle_conn(req: Request<hyper::body::Incoming>, cache: &Cac
     // define response to send to client
     // check if is valid http request (check cache first)
     if req.method() == hyper::Method::GET {
-        // acquire read lock
-        let guard = cache.read().unwrap();
-        return match guard.get(req.uri()) {
-            Some(http_content) => {
-                let response = Response::builder()
-                    .status(StatusCode::OK)
-                    .header("Content-Type", req.headers().get("Content-Type"))
-                    .body(Body::from(http_content))
-                    .unwrap();
-                Ok(response)
-            }
-            _ => {
-                // if not in cache, check if file exists
-                let (http_content, is_404) = dir_accessor::retrieve_from_path(req.uri());
-                // if it's a 404 error, return that
-                if is_404 {
-                    let response = Response::builder()
-                        .status(StatusCode::NOT_FOUND)
-                        .header("Content-Type", "text/html")
-                        .body(Body::from(http_content))
-                        .unwrap();
-                    Ok(response)
-                } else {
-                    // cache content then send response
-                    let response = Response::builder()
-                        .status(StatusCode::OK)
-                        .header("Content-Type", req.headers().get("Content-Type"))
-                        .body(Body::from(http_content))
-                        .unwrap();
-                    Ok(response)
-                }
-            }
+        let http_content = read_cache(cache, req.uri());
+        if http_content != "Null" {
+            let response = Response::builder()
+                .status(StatusCode::OK)
+                .header("Content-Type", req.headers().get("Content-Type"))
+                .body(Body::from(http_content))
+                .unwrap();
+            return Ok(response)
+        }
+
+        // if not in cache, check if file exists
+        let (http_content, is_404) = dir_accessor::retrieve_from_path(req.uri());
+        // if it's a 404 error, return that
+        if is_404 {
+            let response = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .header("Content-Type", "text/html")
+                .body(Body::from(http_content))
+                .unwrap();
+            return Ok(response)
+        } else {
+            // cache content then send response
+            write_to_cache(cache, req.uri(), http_content);
+            let response = Response::builder()
+                .status(StatusCode::OK)
+                .header("Content-Type", req.headers().get("Content-Type"))
+                .body(Body::from(http_content))
+                .unwrap();
+            return Ok(response)
         }
     }
+    // Otherwise send bad request
+    let response = Response::builder()
+        .status(StatusCode::BAD_REQUEST)
+        .body(Body::empty())
+        .unwrap();
+    return Ok(response)
+}
 
-    fn read_cache(cache: Cache, uri: Uri) -> String {
-        let guard = cache.read().unwrap();
-        return match guard.get(uri) {
-            Some(http_content) => http_content.clone(),
-            _ => "Null".to_string()
-        }
+fn read_cache(cache: &Cache, uri: &Uri) -> String {
+    let guard = cache.read().unwrap();
+    return match guard.get(uri) {
+        Some(http_content) => http_content.clone(),
+        _ => "Null".to_string()
     }
+}
 
-    async fn write_to_cache(cache: Cache, uri: Uri, ) {
-
-    }
-
-    // if doesn't exist, send 404
+fn write_to_cache(cache: &Cache, uri: &Uri, http_content: String) {
+    let guard = cache.write().unwrap();
+    guard.insert(uri, http_content);
 }
--- src/dir_accessor.rs
+++ src/dir_accessor.rs
@@ -1,7 +1,8 @@
+use hyper::Uri;
 use tokio::fs;
 
 // returns http contents and if the contents are a 404 page or not
-pub(crate) async fn retrieve_from_path(path: &str) -> (String, bool) {
+pub(crate) async fn retrieve_from_path(path: &Uri) -> (String, bool) {
     // check if file exists
 
 }
