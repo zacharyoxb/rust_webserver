6a6b04341f2c64466ef4b7a202f62693e80011eb
zacharyoxb <oxboroughzachary@protonmail.com>
2024-05-31 20:33:54 +0100

    Major bug fixes (correct body composition)

--- src/connection_handler.rs
+++ src/connection_handler.rs
@@ -7,14 +7,14 @@ use crate::{Cache, dir_accessor};
 
 pub(crate) async fn handle_conn(req: Request<hyper::body::Incoming>, cache: &Cache) -> Result<Response<Full<Bytes>>, Infallible> {
     // define response to send to client
-    // check if is valid http request (check cache first)
+    // check request type
     if req.method() == hyper::Method::GET {
         let http_content = read_cache(cache, req.uri());
         if http_content != "Null" {
             let response = Response::builder()
                 .status(StatusCode::OK)
                 .header("Content-Type", req.headers().get("Content-Type").unwrap())
-                .body(Body::from(http_content)) // Ok(Response::new(Body::from("Hello world")))
+                .body(Full::new(Bytes::from(http_content)))
                 .unwrap();
             return Ok(response)
         }
@@ -22,22 +22,22 @@ pub(crate) async fn handle_conn(req: Request<hyper::body::Incoming>, cache: &Cac
         // if not in cache, check if file exists
         let (http_content, is_404) = dir_accessor::retrieve_from_path(req.uri()).await;
         // if it's a 404 error, return that
-        if is_404 {
+        return if is_404 {
             let response = Response::builder()
                 .status(StatusCode::NOT_FOUND)
                 .header("Content-Type", "text/html")
-                .body(Body::from(http_content))
+                .body(Full::new(Bytes::from(http_content)))
                 .unwrap();
-            return Ok(response)
+            Ok(response)
         } else {
             // cache content then send response
-            write_to_cache(cache, req.uri(), http_content);
+            write_to_cache(cache, req.uri(), &http_content);
             let response = Response::builder()
                 .status(StatusCode::OK)
-                .header("Content-Type", req.headers().get("Content-Type"))
-                .body(Body::from(http_content))
+                .header("Content-Type", req.headers().get("Content-Type").unwrap())
+                .body(Full::new(Bytes::from(http_content)))
                 .unwrap();
-            return Ok(response)
+            Ok(response)
         }
     }
     // Otherwise send bad request
@@ -56,7 +56,7 @@ fn read_cache(cache: &Cache, uri: &Uri) -> String {
     }
 }
 
-fn write_to_cache(cache: &Cache, uri: &Uri, http_content: String) {
+fn write_to_cache(cache: &Cache, uri: &Uri, http_content: &String) {
     let guard = cache.write().unwrap();
     guard.insert(uri, http_content);
 }
