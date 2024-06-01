11c24f62b3a19c5a2b9fabc21f42544ba0db2ddb
zacharyoxb <oxboroughzachary@protonmail.com>
2024-06-01 17:54:02 +0100

    Added template call for all the methods I will implement

--- src/request_handler/mod.rs
+++ src/request_handler/mod.rs
@@ -1,3 +1,9 @@
-pub mod connection_handler;
 pub mod get_handler;
+pub mod options_handler;
+pub mod head_handler;
+pub mod post_handler;
+pub mod put_handler;
+pub mod delete_handler;
+pub mod trace_handler;
+pub mod connect_handler;
 
