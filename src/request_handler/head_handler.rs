0d1a395a6c6f3dd2b47395f65fb9c8ba99b72e11
zacharyoxb <oxboroughzachary@protonmail.com>
2024-06-02 14:17:02 +0100

    Options done, head started

--- src/request_handler/head_handler.rs
+++ src/request_handler/head_handler.rs
@@ -4,12 +4,30 @@ use http_body_util::Full;
 // External crate imports
 use hyper::{Request, Response, StatusCode};
 use hyper::body::Bytes;
+use chrono::offset;
+use chrono::format::StrftimeItems;
+use sysinfo::System;
 
 // Handles option requests, returning either a option response packet or server error packet
 pub(crate) async fn handle_head(_req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    // TODO: When adding handlers, change Allow header
     let response = Response::builder()
-        .status(StatusCode::NOT_IMPLEMENTED)
+        .status(StatusCode::OK)
+        .header("Date", get_time())
+        .header("Server", get_server_version())
+        .header("Allow", "GET, OPTIONS, HEAD")
         .body(Full::new(Bytes::new()))
         .unwrap();
     return Ok(response)
+}
+
+fn get_time() -> String {
+    // Standard HTTP format: Date: Sun, 02 Jun 2024 12:00:00 UTC
+    let date_time = offset::Utc::now();
+    let date_format = StrftimeItems::new("%a, %d %b %Y %H:%M:%S UTC");
+    date_time.format_with_items(date_format.clone()).to_string()
+}
+
+fn get_server_version() -> String {
+    System::os_version().unwrap_or_else(|| "Unknown version".to_string())
 }
--- src/request_handler/options_handler.rs
+++ src/request_handler/options_handler.rs
@@ -7,8 +7,14 @@ use hyper::body::Bytes;
 
 // Handles option requests, returning either a option response packet or server error packet
 pub(crate) async fn handle_option(_req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    //TODO: When completing handlers, add to the headers
     let response = Response::builder()
-        .status(StatusCode::NOT_IMPLEMENTED)
+        .status(StatusCode::NO_CONTENT)
+        .header("Allow", "GET, OPTIONS") 
+        .header("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD")
+        .header("Access-Control-Allow-Headers", "Content-Type")
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Credentials", "true")
         .body(Full::new(Bytes::new()))
         .unwrap();
     return Ok(response)
