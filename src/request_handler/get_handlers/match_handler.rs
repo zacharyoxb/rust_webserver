7ead22d6659af7eeb432f6c9bcce2699fc3b2875
zacharyoxb <oxboroughzachary@protonmail.com>
2024-06-03 22:55:26 +0100

    minor change to avoid warning

--- src/request_handler/get_handlers/match_handler.rs
+++ src/request_handler/get_handlers/match_handler.rs
@@ -4,14 +4,11 @@ use std::sync::Arc;
 use http_body_util::Full;
 // External crate imports
 use hyper::body::Bytes;
-use hyper::{Request, Response, StatusCode};
+use hyper::{Request, Response};
 use crate::Cache;
+use crate::request_handler::handler_utils;
 
 //TODO: handle match then handle both modified/match at the same time by calling both in main and analysing the responses
-pub(crate) async fn handle_match(req: Request<hyper::body::Incoming>, cache: Arc<Cache>) -> Result<Response<Full<Bytes>>, Infallible> {
-    let response = Response::builder()
-        .status(StatusCode::NOT_IMPLEMENTED)
-        .body(Full::new(Bytes::new()))
-        .unwrap();
-    return Ok(response)
+pub(crate) async fn handle_match(_req: Request<hyper::body::Incoming>, _cache: Arc<Cache>) -> Result<Response<Full<Bytes>>, Infallible> {
+    handler_utils::send_not_implemented_packet()
 }
