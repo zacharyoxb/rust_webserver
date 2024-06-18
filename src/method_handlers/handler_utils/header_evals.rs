76ea7dd4a60022d3947e9c1101a857f1dcb5e950
zacharyoxb <oxboroughzachary@protonmail.com>
2024-06-18 16:42:22 +0100

    If-Range etag detection fix

--- src/method_handlers/handler_utils/header_evals.rs
+++ src/method_handlers/handler_utils/header_evals.rs
@@ -46,9 +46,8 @@ pub(crate) fn if_range(
     return if let Some(if_range_some) = if_range_header {
         // convert to str
         if let Ok(header_str) = if_range_some.to_str() {
-            // TODO: etags get here but don't go into the right if. The range any is probably wrong
             // check if etag or date
-            return if (0..3).any(|i| &header_str[i..i + 1] == "\"") {
+            return if !header_str.contains(',') {
                 // check if etag matches
                 strong_compare(if_range_some, etag)
             } else if let (Some(date), Some(one_sec)) = (date_validator, TimeDelta::new(1, 0)) {
