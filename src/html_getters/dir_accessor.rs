cee0bd24e637dcfb938e73d35550cfa917452e0b
zacharyoxb <oxboroughzachary@protonmail.com>
2024-06-19 17:32:39 +0100

    SystemTime last_modified rounded on retrieval to prevent date val problems

--- src/html_getters/dir_accessor.rs
+++ src/html_getters/dir_accessor.rs
@@ -2,7 +2,8 @@
 use hyper::body::Bytes;
 use std::env;
 use std::path::PathBuf;
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
+use chrono::{DateTime, Timelike, Utc};
 // External crate imports
 use hyper::Uri;
 use tokio::fs;
@@ -24,9 +25,17 @@ pub(crate) async fn retrieve_resource(uri: &Uri) -> Result<(Bytes, Option<System
     let path_exists = path.try_exists()?;
     match path_exists {
         true => {
+            // read the content and get the last modified in SystemTime
             let http_content = fs::read_to_string(path.clone()).await?;
             let last_modified = fs::metadata(path.clone()).await?.modified()?;
-            let return_data = (Bytes::from(http_content), Some(last_modified));
+            
+            // convert SystemTime to DateTime then round/convert back
+            let datetime_last_mod: DateTime<Utc> = DateTime::from(last_modified);
+            let datetime_trunc = datetime_last_mod.with_nanosecond(0).unwrap();
+            let last_modified_rounded = SystemTime::UNIX_EPOCH + Duration::new(datetime_trunc.timestamp() as u64, 0);
+            
+            // form tuple and send off
+            let return_data = (Bytes::from(http_content), Some(last_modified_rounded));
             Ok(return_data)
         }
         false => {
